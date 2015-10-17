45631316d177a2cd76b92683f68604cc151a11c1
Kathy <Kathy.20@hotmail.es>
2015-10-17 15:50:16 -0600

    Latex Arboles Binarios

--- io1/ArbolesBinarios/logicaArbolesB.c
+++ io1/ArbolesBinarios/logicaArbolesB.c
@@ -12,6 +12,7 @@ int ArbolesBBeamer(void){
 
     fprintf(fp,"\\documentclass[10]{beamer} \n");
     fprintf(fp,"\\usepackage[T1]{fontenc} \n");
+    fprintf(fp,"\\usepackage{lmodern}\n");
     fprintf(fp,"\\usepackage[spanish]{babel}\n");
     fprintf(fp,"\\usepackage{fancyhdr}\n");
     fprintf(fp,"\\usepackage{xcolor}\n");
@@ -33,7 +34,7 @@ int ArbolesBBeamer(void){
     fprintf(fp,"\\title{{\\color{WHITE} \\large \\textbf{INSTITUTO TECNOL\\'OGICO DE COSTA RICA}} \\\\ \\vspace{0.02cm} \n");
     fprintf(fp,"{\\color{WHITE} \\large \\textbf{ESCUELA DE INGENIER\\'IA EN COMPUTACI'ON }} \\\\ \\vspace{0.02cm} \n");
     fprintf(fp,"{\\color{WHITE} \\large \\textbf{INVESTIGACI\\'ON DE OPERACIONES  }} \\\\ \\vspace{0.02cm} \n");
-    fprintf(fp,"{\\color{WHITE} \\large \\textbf{ALGORITMO DE LA MOCHILA - KNAPSACK  }} \\\\ \\vspace{0.02cm} \n");
+    fprintf(fp,"{\\color{WHITE} \\large \\textbf{ALGORITMO DE A\\'RBOLES BINARIOS DE B\\'USQUEDAS \\'OPTIMAS  }} \\\\ \\vspace{0.02cm} \n");
     fprintf(fp,"{\\color{WHITE} \\large \\textbf{I SEMESTRE  }}\\\\ \\vspace{0.02cm}\n"); 
     fprintf(fp,"{\\color{WHITE} \\large \\textbf{PROFESOR}} \\\\ \\vspace{0.02cm}\n"); 
     fprintf(fp,"{\\color{WHITE} \\large DR. FRANCISCO J. TORRES ROJAS  } \\\\ \\vspace{0.02cm}\n"); 
@@ -49,21 +50,32 @@ int ArbolesBBeamer(void){
 
     fprintf(fp,"\\begin{frame}\n");
     fprintf(fp,"\\color{white}\n");
-    fprintf(fp,"\\frametitle{ALGORITMO DE LA MOCHILA}\n");
-    fprintf(fp,"El algoritmo de Floyd-Warshall, descrito en 1959 por Bernard Roy.\n");
-    fprintf(fp,"\\\\Es un algoritmo de análisis sobre grafos para encontrar el camino mínimo en grafos dirigidos ponderados.\n"); 
-    fprintf(fp,"\\\\El algoritmo encuentra el camino entre todos los pares de vértices en una única ejecución.\n");
-    fprintf(fp,"\\\\ El algoritmo de Floyd-Warshall es un ejemplo de programación dinámica.\n");
+    fprintf(fp,"\\frametitle{ALGORITMO DE A\\'RBOLES BINARIOS DE B\\'USQUEDAS \\'OPTIMAS}\n");
+    fprintf(fp,"Tambi\\'en llamados BST (acr\\'onimo del ingl\\'es Binary Search Tree).\n");
+    fprintf(fp,"\\\\ Tipo particular de \\'arbol binario que presenta una estructura de datos en forma de \\'arbol .\n"); 
+    fprintf(fp,"\\\\Un \\'arbol binario no vac\\'io, de ra\\'iz R.\n");
+    fprintf(fp,"\\\\ Existe una relaci\\'on de orden establecida entre los elementos de los nodos.\n");
     fprintf(fp,"\\end{frame} \n");
 
     fprintf(fp,"\\begin{frame}\n");
     fprintf(fp,"\\color{white}\n");
-    fprintf(fp,"\\frametitle{ROBERT W. FLOYD}\n");
-    fprintf(fp,"Cient\\'ifico de la computaci\\'on USA (1936-2001)\\\\\n");
-    fprintf(fp,"Bachiller en Artes Liberales y F\\'isica. \\\\ Operador de computadoras (1960). \\\\ Gan\\'o un premio Turing en 1978.\n");
+    fprintf(fp,"\\frametitle{CONDICIONES DE UN \\'ARBOL BINARIO}\n");
+    fprintf(fp,"En caso de tener sub\\'arbol izquierdo, la ra\\'iz R debe ser mayor que el valor m\\'aximo almacenado en el sub\\'arbol izquierdo, y que el sub\\'arbol izquierdo sea un \\'arbol binario de b\\'usqueda.\n");
     fprintf(fp,"\\end{frame} \n");
 
-    fprintf(fp,"\\begin{frame}\\frametitle{KnapSack}\n ");
+    fprintf(fp,"\\begin{frame}\n");
+    fprintf(fp,"\\color{white}\n");
+    fprintf(fp,"\\frametitle{CONDICIONES DE UN \\'ARBOL BINARIO - CONT}\n");
+    fprintf(fp,"En caso de tener sub\\'arbol derecho, la ra\\'iz R debe ser menor que el valor m\\'inimo almacenado en el sub\\'arbol derecho, y que el sub\\'arbol derecho sea un \\'arbol binario de b\\'usqueda.\n");
+    fprintf(fp,"\\end{frame} \n");
+
+    fprintf(fp,"\\begin{frame}\n");
+    fprintf(fp,"\\color{white}\n");
+    fprintf(fp,"\\frametitle{PRINCIPIO DE OPTIMALIDAD}\n");
+    fprintf(fp,"Todos los sub\\'arboles de un \\'arbol \\'optimo son \\'optimos con respecto a las claves que contienen.\n");
+    fprintf(fp,"\\end{frame} \n");
+
+    fprintf(fp,"\\begin{frame}\\frametitle{VALORES INICIALES}\n ");
     fprintf(fp,"\\color{white}\n");
    
     fprintf(fp,"\\end{frame} \n");
